3b9de62c43c1fe5fb1ff81ff3dcf623562c279ee
Susi <susl@rickroll.link>
2021-08-07 14:21:34 +0200

    make ccc.js return multiple pages

--- feeds/ccc.js
+++ feeds/ccc.js
@@ -5,18 +5,35 @@ const fetch = require('node-fetch');
 
 var feed = []
 
-fetch('https://www.ccc.de/de/')
-    .then(res => res.text())
-    .then(body => {
+updateFeed = async () => {
+  for (var i = 0; i <= 100; i++) {
+    try {
+      const res = await fetch(`https://www.ccc.de/de/?page=${i}`);
+      const body = await res.text()
+      let feedpart = []
+
       let $ = cheerio.load(body)
-      $('.article_partial').each((i, e) => {
-        feed.unshift({
+      $('.article_partial').each((ii, e) => {
+        feedpart.push({
           headline: e.children[1].children[0].children[0].data,
-          content: e.children[5].children[0].data
-        })
-      })
-    });
+          content: e.children[5].children[0].data + "(page " + i + ")"
+        });
+      });
+
+      feed.push(feedpart)
+      feed = feed.flat()
+
+      console.log(i)
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+updateFeed()
+
+
 
 module.exports = function () {
-  return feed;
+  return [...feed].reverse();
 };
